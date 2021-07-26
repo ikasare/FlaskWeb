8bf7ebda69a508309d7aa275581c4e00781797f5
Isaac Asare <iasarekwaku@gmail.com>
2021-07-26 19:38:30 +0000

    added styling

--- audio.py
+++ audio.py
@@ -1,9 +1,13 @@
 import speech_recognition as sr
 from os import path
-                
+
+
 def printWAV(FILE_NAME, pos, clip):
     # use the audio file as the audio source
-    AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), 'static/'+FILE_NAME)
+    AUDIO_FILE = path.join(
+        path.dirname(
+            path.realpath(__file__)),
+        'static/' + FILE_NAME)
     r = sr.Recognizer()
     text = ""
     with sr.AudioFile(AUDIO_FILE) as source:
@@ -14,6 +18,5 @@ def printWAV(FILE_NAME, pos, clip):
         except sr.UnknownValueError:
             text += "Could not understand audio\n"
         except sr.RequestError as e:
-            text += "Could not request results; {0}".format(e)+ "\n"
+            text += "Could not request results; {0}".format(e) + "\n"
     return text
-
--- demo.py
+++ demo.py
@@ -3,7 +3,9 @@ from flask_bcrypt import Bcrypt
 from forms import RegistrationForm, LoginForm
 from flask_sqlalchemy import SQLAlchemy
 from audio import printWAV
-import time, random, threading
+import time
+import random
+import threading
 from turbo_flask import Turbo
 import json
 
@@ -14,20 +16,20 @@ app.config['SECRET_KEY'] = '58cb11e106953683a34325b53dfd9c30'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
-interval=10
+interval = 10
 FILE_NAME = "siratt.wav"
 turbo = Turbo(app)
 
 
 class User(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  username = db.Column(db.String(20), unique=True, nullable=False)
-  email = db.Column(db.String(120), unique=True, nullable=False)
-  password = db.Column(db.String(60), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
 
-  def __repr__(self):
-    return f"User('{self.username}', '{self.email}')"
-  
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+
 # def test():
 #   user = User.query.filter_by(email='cap@case.edu').all()
 #   another = User.query.filter_by(email='fire@case.edu')
@@ -35,86 +37,101 @@ class User(db.Model):
 #   return user[0].password, another[0].password, pw_hash
 
 # print(test())
-  
-  
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-  return render_template('home.html', subtitle='Home Page', text='This is the home page')
+    return render_template(
+        'home.html',
+        subtitle='Home Page',
+        text='This is the home page')
 
 
 @app.route("/second_page")
 def second_page():
-    return render_template('second_page.html', subtitle='Second Page', text='This is the second page')
+    return render_template(
+        'second_page.html',
+        subtitle='Second Page',
+        text='This is the second page')
 
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
-    if form.validate_on_submit():# checks if entries are valid
-      try:
-          pw_hash = bcrypt.generate_password_hash(form.password.data.encode('utf-8'))
-          user = User(username=form.username.data, email=form.email.data, password=pw_hash)
-          db.session.add(user)
-          db.session.commit()
+    if form.validate_on_submit():  # checks if entries are valid
+        try:
+            pw_hash = bcrypt.generate_password_hash(
+                form.password.data.encode('utf-8'))
+            user = User(
+                username=form.username.data,
+                email=form.email.data,
+                password=pw_hash)
+            db.session.add(user)
+            db.session.commit()
 
-      except Exception as e:
-        flash(f'Your account could not be created. This username and/or email already exists!')
-      else:
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home')) # if so - send to home page
+        except Exception as e:
+            flash(
+                f"""Your account could not be created.
+                This username and/or email already exists!""")
+        else:
+            flash(f'Account created for {form.username.data}!', 'success')
+            return redirect(url_for('home'))  # if so - send to home page
     return render_template('register.html', title='Register', form=form)
-    
-  
+
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    if form.validate_on_submit(): # checks if entries are valid
+    if form.validate_on_submit():  # checks if entries are valid
         user = User.query.filter_by(email=form.email.data).all()
         if not user:
-          flash(f'User not found {form.email.data}!')
-          return render_template('login.html', title='Login', form=form)
-        if not bcrypt.check_password_hash(user[0].password, form.password.data):
-          flash(f'Incorrect password for {form.email.data}!')
-          return render_template('login.html', title='Login', form=form)
+            flash(f'User not found {form.email.data}!')
+            return render_template('login.html', title='Login', form=form)
+        if not bcrypt.check_password_hash(
+                user[0].password, form.password.data):
+            flash(f'Incorrect password for {form.email.data}!')
+            return render_template('login.html', title='Login', form=form)
         flash(f'Logged In {form.email.data}!', 'success')
-        return render_template('captions.html', songName="Sir Att Narrates Hello", file=FILE_NAME) # if so - send to captions page
+        return render_template(
+            'captions.html',
+            songName="Sir Att Narrates Hello",
+            file=FILE_NAME)  # if so - send to captions page
     return render_template('login.html', title='Login', form=form)
-    
 
-  
+
 @app.route("/captions")
 def captions():
     TITLE = "Sir Att Narrates Hello"
     return render_template('captions.html', songName=TITLE, file=FILE_NAME)
-  
+
+
 @app.before_first_request
 def before_first_request():
-    #resetting time stamp file to 0
-    file = open("pos.txt","w") 
+    # resetting time stamp file to 0
+    file = open("pos.txt", "w")
     file.write(str(0))
     file.close()
 
-    #starting thread that will time updates
+    # starting thread that will time updates
     threading.Thread(target=update_captions, daemon=True).start()
+
 
 @app.context_processor
 def inject_load():
     # getting previous time stamp
-    file = open("pos.txt","r")
+    file = open("pos.txt", "r")
     pos = int(file.read())
     file.close()
 
     # writing next time stamp
-    file = open("pos.txt","w")
-    file.write(str(pos+interval))
+    file = open("pos.txt", "w")
+    file.write(str(pos + interval))
     file.close()
 
-    #returning captions
-    return {'caption':printWAV(FILE_NAME, pos=pos, clip=interval)}
+    # returning captions
+    return {'caption': printWAV(FILE_NAME, pos=pos, clip=interval)}
+
 
 def update_captions():
     with app.app_context():
@@ -123,8 +140,11 @@ def update_captions():
             time.sleep(interval)
 
             # forcefully updating captionsPane with caption
-            turbo.push(turbo.replace(render_template('captionsPane.html'), 'load'))  
-  
+            turbo.push(
+                turbo.replace(
+                    render_template('captionsPane.html'),
+                    'load'))
+
 
 if __name__ == '__main__':
-  app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0")
